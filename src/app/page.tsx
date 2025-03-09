6fb576d3b03f3682e1fdca729587bb191dd2e815
syahrial6 <syr06rial@gmail.com>
2025-03-09 15:53:24 +0700

    v1.1.2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,49 @@
-'use client'
+"use client";
 import About from "./components/About";
 import CursorBlur from "./components/CursorBlur";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Project from "./components/Project";
-import { useState, useEffect } from "react";
+import { useState, useEffect,useRef } from "react";
 import { motion } from "framer-motion";
+import gsap from "gsap";
 // import { useEffect } from "react";
 
-
-const LoadingOverlay = ({ setLoading }) => {
+type LoadingOverlayProps = {
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+};
+const LoadingOverlay: React.FC<LoadingOverlayProps> = ({ setLoading }) => {
   const [split, setSplit] = useState(false);
+  const boxRef = useRef(null);
 
   useEffect(() => {
     setTimeout(() => {
       setSplit(true);
       setTimeout(() => setLoading(false), 1000); // Setelah animasi selesai, hilangkan overlay
     }, 1500); // Durasi sebelum gerbang terbuka
+  }, []);
+
+  useEffect(() => {
+    gsap.fromTo(
+      boxRef.current,
+      {
+        opacity: 0,
+        scale: 0,
+        y: 80,
+        rotationX: 180,
+        transformOrigin: "0% 50% -50",
+      },
+      {
+        duration: 0.8,
+        opacity: 1,
+        scale: 1,
+        y: 0,
+        rotationX: 0,
+        ease: "back.out(1.7)",
+        stagger: 0.01,
+      }
+    );
   }, []);
 
   return (
@@ -29,10 +55,11 @@ const LoadingOverlay = ({ setLoading }) => {
     >
       {/* Text Loading */}
       <motion.p
+      ref={boxRef}
         initial={{ opacity: 1 }}
         animate={{ opacity: split ? 0 : 1 }}
         transition={{ duration: 0.4 }}
-        className="text-center mb-12 font-primary font-bold text-3xl"
+        className="text-center mb-12 font-primary font-bold text-4xl"
       >
         Loading...
       </motion.p>
@@ -42,23 +69,31 @@ const LoadingOverlay = ({ setLoading }) => {
         initial={{ width: "0%" }}
         animate={{ width: "100%" }}
         transition={{ duration: 1, ease: "easeInOut" }}
-        className="bg-black h-[10px] absolute"
+        className="bg-black h-[5px] absolute"
       />
 
       {/* Gerbang Atas */}
       <motion.div
-        initial={{ top: "50%", height: "5px", width: "100%", opacity: 0 }}
-        animate={split ? { top: "0%", height: "50vh", opacity: 1 } : {}}
+        initial={{ scaleX: 0, height: "5px", top: "50%", originX: 0.5 }}
+        animate={
+          split
+            ? { top: "0%", height: "50vh", scaleX: 1 } // Naik ke atas & tinggi bertambah
+            : { scaleX: 1 } // Melebar dulu
+        }
         transition={{ duration: 1, ease: "easeInOut" }}
-        className="bg-black absolute"
+        className="bg-black absolute w-full"
       />
 
       {/* Gerbang Bawah */}
       <motion.div
-        initial={{ bottom: "50%", height: "5px", width: "100%", opacity: 0 }}
-        animate={split ? { bottom: "0%", height: "50vh", opacity: 1 } : {}}
+        initial={{ scaleX: 0, height: "5px", top: "50%", originX: 0.5 }}
+        animate={
+          split
+            ? { top: "0%", height: "100vh", scaleX: 1 } // Naik ke atas & tinggi bertambah
+            : { scaleX: 1 } // Melebar dulu
+        }
         transition={{ duration: 1, ease: "easeInOut" }}
-        className="bg-black absolute"
+        className="bg-black absolute w-full"
       />
     </motion.div>
   );
@@ -75,18 +110,21 @@ export default function Home() {
   // }, []); // Dependency array kosong biar jalan sekali saat pertama render
   return (
     <div className="bg-black no-scrollbar overflow-y-scroll h-screen">
-      {loading ? <LoadingOverlay setLoading={setLoading} />:(<div>
+      {loading ? (
+        <LoadingOverlay setLoading={setLoading} />
+      ) : (
+        <div>
+          <CursorBlur />
+          <Navbar />
 
-      <CursorBlur/>
-      <Navbar />
-      
-      <Hero />
-      <div className="lg:my-12 my-24">
-      <About/>
-      </div>
-      <Project/>
-      <Footer/>
-      </div>)}
+          <Hero />
+          <div className="lg:my-12 my-24">
+            <About />
+          </div>
+          <Project />
+          <Footer />
+        </div>
+      )}
     </div>
   );
 }
